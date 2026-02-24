ff4ef1f9d39ee673631ac7f4fb98a0316822c193
Mikael Klasson <43577743+emklasson@users.noreply.github.com>
2026-02-24 21:49:05 +0900

    fix: use all msieve factors

--- dodc_msieve.cc
+++ dodc_msieve.cc
@@ -8,39 +8,42 @@ using namespace std;
 /// @param wu The workunit to run msieve on.
 /// @return True if a factor was found, otherwise false.
 bool do_workunit_msieve(workunit_t &wu) {
+    wu.cmdline = "msieve -t 1 -p -l " + wu.tempfile + ".log -s " + wu.tempfile + ".dat" + " " + wu.inputnumber;
+
+    if (wu.schedule_bg) {
+        // setpriority( PRIO_DARWIN_PROCESS, 0, PRIO_DARWIN_BG ); // Only schedules on efficiency cores.
+        wu.cmdline = "./schedule_bg '" + wu.cmdline + "'";
+    }
+
+    if (!spawn_and_wait(wu.cmdline).first) {
+        log("ERROR: Failed spawning msieve.\n");
+        return false;
+    }
+
+    ifstream ftmp(wu.tempfile + ".log");
     string line;
     bool foundfactor = false;
-    while (!foundfactor) {
-        wu.cmdline = "msieve -t 1 -p -l " + wu.tempfile + ".log -s " + wu.tempfile + ".dat" + " " + wu.inputnumber;
+    while (getline(ftmp, line)) {
+        auto pos = line.find(wu.inputnumber);
+        if (pos != line.npos) {
+            wu.result.method = "MSIEVE_QS";
+            wu.result.args = "";
+            wu.result.factor = "";
 
-        if (wu.schedule_bg) {
-            // setpriority( PRIO_DARWIN_PROCESS, 0, PRIO_DARWIN_BG ); // Only schedules on efficiency cores.
-            wu.cmdline = "./schedule_bg '" + wu.cmdline + "'";
-        }
-
-        if (!spawn_and_wait(wu.cmdline).first) {
-            log("ERROR: Failed spawning msieve.\n");
-            return false;
-        }
-
-        ifstream ftmp(wu.tempfile + ".log");
-        while (getline(ftmp, line)) {
-            auto pos = line.find(wu.inputnumber);
-            if (pos != line.npos) {
-                wu.result.method = "MSIEVE_QS";
-                wu.result.args = "";
-
-                while (getline(ftmp, line)) {
-                    pos = line.find("factor:");
-                    if (pos != line.npos) {
-                        stringstream ss(line.substr(pos + 7));
-                        ss >> ws >> wu.result.factor;
-                        foundfactor = true;
-                        break;
-                    }
+            // Find and add all factors for this input number.
+            while (getline(ftmp, line)) {
+                pos = line.find("factor:");
+                if (pos != line.npos) {
+                    stringstream ss(line.substr(pos + 7));
+                    string factor;
+                    ss >> ws >> factor;
+                    wu.result.factor += (foundfactor ? " " : "") + factor;
+                    foundfactor = true;
+                } else if (foundfactor) {
+                    break;
                 }
-                break;
             }
+            break;
         }
     }
 
